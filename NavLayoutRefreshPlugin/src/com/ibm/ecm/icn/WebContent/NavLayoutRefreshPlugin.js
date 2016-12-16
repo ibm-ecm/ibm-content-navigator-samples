3b8a92d8704f6b9c22f747187348b5d30a52ab01
garcia323 <ggarcia1@us.ibm.com>
2016-12-16 15:13:49 -0800

    Added the plugin's main JavaScript and CSS files

---
+++ NavLayoutRefreshPlugin/src/com/ibm/ecm/icn/WebContent/NavLayoutRefreshPlugin.js
@@ -0,0 +1,33 @@
+/*
+ * Licensed Materials - Property of IBM (c) Copyright IBM Corp. 2016  All Rights Reserved.
+ * 
+ * US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule Contract with
+ * IBM Corp.
+ * 
+ * DISCLAIMER OF WARRANTIES :
+ * 
+ * Permission is granted to copy and modify this Sample code, and to distribute modified versions provided that both the
+ * copyright notice, and this permission notice and warranty disclaimer appear in all copies and modified versions.
+ * 
+ * THIS SAMPLE CODE IS LICENSED TO YOU AS-IS. IBM AND ITS SUPPLIERS AND LICENSORS DISCLAIM ALL WARRANTIES, EITHER
+ * EXPRESS OR IMPLIED, IN SUCH SAMPLE CODE, INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL IBM OR ITS LICENSORS OR SUPPLIERS BE LIABLE FOR
+ * ANY DAMAGES ARISING OUT OF THE USE OF OR INABILITY TO USE THE SAMPLE CODE, DISTRIBUTION OF THE SAMPLE CODE, OR
+ * COMBINATION OF THE SAMPLE CODE WITH ANY OTHER CODE. IN NO EVENT SHALL IBM OR ITS LICENSORS AND SUPPLIERS BE LIABLE
+ * FOR ANY LOST REVENUE, LOST PROFITS OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE
+ * DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, EVEN IF IBM OR ITS LICENSORS OR SUPPLIERS HAVE
+ * BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
+
+require(["dojo/_base/declare",
+         "ecm/model/Desktop",
+         "ecm/widget/layout/BrowsePane"
+], function(declare, Desktop, BrowsePane) {
+	
+	// Override the desktop's bookmark layout with the layout provided by the plug-in
+	Desktop.bookmarkLayout = "iNOCLayoutPluginDojo/BookmarkLayout";
+	
+	// Change the CSS class selector of the Browse feature pane to give it a custom look
+	BrowsePane.prototype.baseClass = "iNOCBrowsePane";
+	
+});
