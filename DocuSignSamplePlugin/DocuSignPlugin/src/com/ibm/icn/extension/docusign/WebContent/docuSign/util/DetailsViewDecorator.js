f27edd98ed22b656f32711ab932dc206c62afe0b
Balunaini Prasad <balunaini@Balunainis-MacBook-Pro.local>
2017-05-24 13:51:57 +0530

    include both versions of decorator code i.e., multiState (default), and dedicated column

--- DocuSignSamplePlugin/DocuSignPlugin/src/com/ibm/icn/extension/docusign/WebContent/docuSign/util/DetailsViewDecorator.js
+++ DocuSignSamplePlugin/DocuSignPlugin/src/com/ibm/icn/extension/docusign/WebContent/docuSign/util/DetailsViewDecorator.js
@@ -5,45 +5,40 @@
 
 define([
 	"dojo/_base/lang",
-	"dojo/_base/declare"
-], function(lang, declare) {
-
-	var Utils = declare("docuSign.util.DetailsViewDecorator", [], {
-		
-		docuSignPluginStatusDecorator: function(data, rowId, rowIndex) 
-		{	
-			var returnTag = "";
-			var item = this.grid.row(rowId).item();
-			
-			if (data) 
-			{
-				if(data == "Sent"){
-					returnTag = '<img class="ecmStatusIcon ecmSignSentIcon" alt="Sent for signature" title="Sent for signature" src="' + this._blankGif + '" />';
-					returnTag += '<div class="dijitHidden">Sent for signature</div>';
-					
-					return returnTag;
-				}
-				else if (data == "Completed"){
-					returnTag = '<img class="ecmStatusIcon ecmSignDoneIcon" alt="Signature Completed" title="Signature Completed" src="' + this._blankGif + '" />';
-					returnTag += '<div class="dijitHidden">Signature Completed</div>';
-					
-					return returnTag;
-				}
-				else if (data == "Checkedin")
-				{
-					returnTag = '<img class="ecmStatusIcon ecmSignCheckedinIcon" alt="Signature Completed and Checkedin" title="Signature Completed and Checkedin" src="' + this._blankGif + '" />';
-					returnTag += '<div class="dijitHidden">Signature Completed and Checkedin</div>';
-					
-					return returnTag;
-				}
-			}
-			else 
-			{
-				return "";
-			}
-		}
-	});
+	"dojo/_base/declare",
+	"dojo/aspect",
+	"ecm/widget/listView/decorators/common",
+	"ecm/widget/listView/decorators/DetailsViewDecorator"
+], function(lang, declare, aspect, common, decorator) {
 	
-	docuSign.util.DetailsViewDecorator = new Utils();
-    return docuSign.util.DetailsViewDecorator;
+	lang.setObject("docuSign.util.DetailsViewDecorator.docuSignPluginStatusDecorator", function(data, rowId, rowIndex) {
+
+		var item = this.grid.row(rowId).item();
+		// get existing state icons for a document 
+		var returnTag = common.multiStateIcon(item, this._states, true);
+		
+		// append signature status icon based on the sign status property value
+		if (data && data.attributeDisplayValues.DSSignatureStatus == "Sent")
+		{
+			returnTag += '<img class="ecmStatusIcon ecmSignSentIcon" alt="Sent for signature" title="Sent for signature" src="' + this._blankGif + '" />';
+			returnTag += '<div class="dijitHidden">Sent for signature</div>';
+			
+            return returnTag;
+		}
+		else if (data && data.attributeDisplayValues.DSSignatureStatus == "Completed")
+		{
+			returnTag += '<img class="ecmStatusIcon ecmSignDoneIcon" alt="Signature Completed" title="Signature Completed" src="' + this._blankGif + '" />';
+			returnTag += '<div class="dijitHidden">Signature Completed</div>';
+			
+			return returnTag;
+		}
+		else if (data && data.attributeDisplayValues.DSSignatureStatus == "Checkedin")
+		{
+			returnTag += '<img class="ecmStatusIcon ecmSignCheckedinIcon" alt="Signature Completed and Checkedin" title="Signature Completed and Checkedin" src="' + this._blankGif + '" />';
+			returnTag += '<div class="dijitHidden">Signature Completed and Checkedin</div>';
+			
+			return returnTag;
+		}
+		
+	});
 });
