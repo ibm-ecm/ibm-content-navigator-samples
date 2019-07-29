1638a3f6333574f9958f1484061157b0aba15027
Andy Choi <andychoi@us.ibm.com>
2019-07-29 11:11:51 -0400

    ICN Dojo NLS Build (#11)
    This Gradle build script can be used to build localized resource bundles for additional languages other than the ones that come with ICN.

---
+++ icn-dojo-nls/nls.profile.js
@@ -0,0 +1,30 @@
+// See http://dojotoolkit.org/reference-guide/build/index.html#build-index
+var profile = {
+	basePath: "build/dojoBuild", // The path of the directory where Dojo Toolkit full source archive file is extracted to
+
+	releaseDir: "dojo/../release",
+	action: "release",
+	layerOptimize: "comments",
+	optimize: "comments",
+	stripConsole: "warn",
+	localeList: "ko,zh,ja", // A comma-separated list of ISO language codes
+
+	packages: [
+		{
+			name: "dojo",
+			location: "./dojo"
+		},
+		{
+			name: "ecm",
+			location: "./ecm"
+		},
+	],
+
+	layers: {
+		"ecm/ecm": {
+			include: [
+				"ecm/ecm"
+			]
+		}
+	}
+};
